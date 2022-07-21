c8b860ae2f6aee98616c17a6d240391e6ebafa71
jonatas.almeida <jonatas.olialmeida@gmail.com>
2022-07-21 15:19:19 -0300

    aula02 PRINT

---
+++ aula02/aula02.py
@@ -0,0 +1,15 @@
+# print('jon', 'snow', sep='.', end='/')
+# print('jonex', 'snower', sep='.')
+
+# Exercicio cpf
+
+# MEU EXEMPLO
+# print('123', end='.')
+# print('456', end='.')
+# print('789', end='-')
+# print('00',)
+
+# EXEMPLO PROFESSOR
+
+print('123', '456', '789', sep='.', end='-')
+print('00')
