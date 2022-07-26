b9e08733a511a18d78ac39b1edf738300cb731d5
jonatas.almeida <jonatas.olialmeida@gmail.com>
2022-07-26 16:13:00 -0300

    aula03

---
+++ aula03_Strings/aula03.py
@@ -0,0 +1,9 @@
+"""
+str - string
+"""
+
+print("Essa é uma 'string' (str)")
+print("Essa é uma \"barra de escape\" (\)")
+print("Esse é meu texto com \n hahaha")
+print(r"Esse é meu texto com \n de verdade hahaha") # r - raw string  << não executa nada dentro da string
+
