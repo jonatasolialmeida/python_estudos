59d288e8105642c449abcd5fc9120dff6da691a4
jonatas.almeida <jonatas.olialmeida@gmail.com>
2022-07-27 09:18:54 -0300

    update

--- aula05_operadores_aritméticos/aula05.py
+++ aula05_operadores_aritméticos/aula05.py
@@ -9,3 +9,25 @@ print('* também serve para repetição', 10 * 'john')
 print('+ além da soma também serve para concatenar strings')
 print('olá' + ' ' + 'john')
 print('voce têm ' + str(10) + ' anos')
+
+# soma
+print(10 + 10)
+
+# subtração
+print(10 - 10)
+
+# multiplicação
+print(10 * 10)
+
+# divisão
+print(10 / 10)
+
+# divisão inteira
+print(10 // 10)
+
+# potenciação
+print(10 ** 10)
+
+# módulo da divisão
+print(10 % 10)
+
