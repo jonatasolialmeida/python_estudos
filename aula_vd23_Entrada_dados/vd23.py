058b0c7c03433fd49f034cfb6c346eec8b2e5213
jonatas.almeida <jonatas.olialmeida@gmail.com>
2022-07-27 11:45:19 -0300

    video 23

---
+++ aula_vd23_Entrada_dados/vd23.py
@@ -0,0 +1,43 @@
+# """
+# Entrada de dados
+# """
+
+# # Entrada de dados
+# # input() -> Recebe uma entrada de dados do usuário
+# # input() --> é uma função
+# input("Digite algo: ")
+
+# # EXEMPLO DE ENTRADA DE DADOS
+# nome = input("Digite seu nome: ")
+# print("Seu nome é: ", nome)
+
+
+# # OUTRO EXEMPLO DE ENTRADA DE DADOS
+# nome1 = input("Digite seu nome: ")
+# idade = input("Digite sua idade: ")
+# nascimento = 2022 - int(idade)
+
+# print()
+# print(f'{nome1} tem {idade} anos. '
+#       f'{nome1} nasceu em {nascimento}')
+
+# print()
+# print()
+
+# # Tipagem de dados
+# # int() -> Converte um valor string para inteiro
+# # float() -> Converte um valor string para float
+# # str() -> Converte um valor inteiro ou float para string
+
+# numero1 = int(input("Digite um número: "))
+# numero2 = input("Digite outro número: ")
+# numero2 = int(numero2)
+
+# print(numero1 + numero2)
+
+var ='5.5'
+out_vsr = int(float(var))
+
+# É poss´vel converter um valor float para inteiro
+# Passará a ser um inteiro (de 5.5 p/ 5)
+print(out_vsr)
